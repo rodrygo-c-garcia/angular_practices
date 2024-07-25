827dae20f6d60f54d7749818ab1b6206e74c7cc1
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-07-25 07:35:17 -0400

    add class

--- 01_typescript-intro/src/main.ts
+++ 01_typescript-intro/src/main.ts
@@ -1,8 +1,11 @@
 import './style.css'
 import { setupCounter } from './counter.ts'
 import { namesList } from './topics/01-basic-types.ts'
-import './topics/03-functions.ts'
-import './topics/04-homework-types.ts'
+// import './topics/03-functions.ts'
+// import './topics/04-homework-types.ts'
+// import './topics/05-basic-destructuring.ts'
+// import './topics/06-function-destructuring.ts'
+import './topics/07-import-export.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
---
+++ 01_typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,10 @@
+export class Person {
+  private _name: string;
+  private _addres: string;
+
+  constructor(name: string, addres: string) {
+    this._name = name;
+    this._addres = addres;
+  }
+
+}
