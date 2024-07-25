5cbc35c0b5c35e4d9c2cf8ad3ca2084f42eb1955
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-07-25 08:17:12 -0400

    add extends

--- 01_typescript-intro/src/topics/08-classes.ts
+++ 01_typescript-intro/src/topics/08-classes.ts
@@ -1,10 +1,38 @@
 export class Person {
-  private _name: string;
-  private _addres: string;
+  constructor(
+    protected _name: string,
+    protected _email: string,
+    protected _addres?: string
+  ) {}
 
-  constructor(name: string, addres: string) {
-    this._name = name;
-    this._addres = addres;
+  getName(): string {
+    return this._name;
   }
 
+  getEmail(): string {
+    return this._email;
+  }
+
+  getAddres(): string | undefined {
+    return this._addres;
+  }
 }
+
+//Clase heroe
+export class Hero extends Person {
+  constructor(
+    protected _name: string,
+    protected _email: string,
+    protected _power: number,
+    protected _addres?: string
+  ) {
+    super(_name, _email, _addres);
+  }
+
+  getPower(): number {
+    return this._power;
+  }
+}
+
+const iroman = new Hero("iroman", "iro@gmail.com", 100, "");
+console.log(iroman.getPower());
