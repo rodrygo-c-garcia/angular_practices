224d39e04748b701c20e554e033a891a0f0e5b3e
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-07-26 08:21:18 -0400

    add function contandor

--- 02_bases/src/app/app.component.ts
+++ 02_bases/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi first page in Angular';
+  public x: number = 10;
+
+  increaseBy(value: boolean): void {
+    value ? this.x++ : this.x--;
+  }
+
+  reseteCounter(): void {
+    this.x = 10;
+  }
 }
