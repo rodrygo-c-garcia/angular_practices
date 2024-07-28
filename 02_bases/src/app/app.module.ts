abcaa9bd057c696cc171361bf78ab730e4820eff
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-07-28 11:11:11 -0400

    add: import and export modules

--- 02_bases/src/app/app.module.ts
+++ 02_bases/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent], // componentes
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HeroComponent, ListComponent], // componentes
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CounterModule], // modulos
   providers: [], // servicios
   bootstrap: [AppComponent], // pagina de inicio
 })
